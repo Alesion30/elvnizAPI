30751376fb999b073425f0934f44042ed16342d5
yuta ohira <alesion90@gmail.com>
2021-05-09 22:21:23 +0900

    add: cocoaを入れている端末のみカウント

--- src/functions/blecount.ts
+++ src/functions/blecount.ts
@@ -6,12 +6,25 @@ export const blecount = async (wait: number = 10): Promise<number> => {
   await obniz.ble.initWait();
   display(`Please wait ${wait} seconds.`);
 
-  const target: BleScanTarget = {};
+  // 接触確認アプリ「COCOA」 ServiceUUID
+  // https://blog.google/documents/58/Contact_Tracing_-_Bluetooth_Specification_v1.1_RYGZbKW.pdf
+  const target: BleScanTarget = {
+    uuids: ["FD6F"]
+  };
   const setting: BleScanSetting = {
     duration: wait,
     duplicate: false,
   };
+
+  // スキャン開始
   const peripherals = await obniz.ble.scan.startAllWait(target, setting);
+
+  peripherals.forEach(peripheral => {
+    console.log(peripheral.rssi);
+  })
+  const count = peripherals.filter(peripheral => peripheral.rssi >= -70).length; // 通信強度を-70dB以上のみカウント
+
+  // スキャン終了
   obniz.ble.scan.endWait();
-  return peripherals.length;
+  return count;
 };
